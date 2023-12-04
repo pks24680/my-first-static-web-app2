4b2d3def62ce407d3c944ce500cd7f694c8e49db
Pruthvi Shah <113222650+pks24680@users.noreply.github.com>
2023-12-05 10:50:43 +1100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import './App.css';
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginForm from './LoginForm';
+import Dashboard from './Dashboard';
+import TableTest from './TableTest';
+import WordTest from './WordCreator';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+
+function App() { 
+  return(
+    <div className='page'>
+      <Routes>
+        <Route path="/" exact element={<LoginForm/>}/>
+        <Route path="/Dashboard" element={<Dashboard/>}/>
+        <Route path="/TableTest" element={<TableTest/>}/>
+        <Route path="/WordTest" element={<WordTest/>}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
